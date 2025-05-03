bff63272269814b46b2b1b8ce869194224223ce3
4z4q <skodrme@gmail.com>
2025-05-04 00:44:03 +0300

    update react-day

--- src/components/booking-components/booking-model.tsx
+++ src/components/booking-components/booking-model.tsx
@@ -142,7 +142,7 @@ export function BookingModal({
             <div className="space-y-4 py-4">
               <div className="space-y-2">
                 <Label htmlFor="date">التاريخ</Label>
-                <Popover modal={true} >
+                <Popover modal={true}>
                   <PopoverTrigger asChild>
                     <Button
                       variant="outline"
